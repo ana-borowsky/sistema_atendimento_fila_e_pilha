9119059369df517d6440c7c60341e4bd70530119
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2025-04-25 15:47:25 -0300

    Altera opcoes para struct

--- src/sistema_atendimento_fila_e_pilha/GeraMenu.java
+++ src/sistema_atendimento_fila_e_pilha/GeraMenu.java
@@ -1,69 +1,61 @@
 package sistema_atendimento_fila_e_pilha;
 
 public class GeraMenu {
-	
-	public String[] opcoes = {
-            "Atendimento", "Solicitações", "Atender", 
-            "Ver fila de atendimento", "Ver próximo da fila", "Passar para atendimento", 
-            "Nova solicitação", "Ver todas as solicitações", "Resposta ao cliente: ", 
-            "ID do cliente: ", "Insira a descrição da solicitação: ", "Data: ", 
-            "Hora: ", "Nome do cliente: ", "Motivo do atendimento: ", 
-            "Deletar", "Ver próxima solicitação"
-			};
-
 
     public void menuPrincipal() {
-        System.out.println("\n|--------MENU--------|");
-        System.out.println("[ 1 ] Atendimento");
-        System.out.println("[ 2 ] Solicitações");
-        System.out.println("[ 3 ] Sair");
-        System.out.println("Sua opção: ");
-    }
-    
-    public void menuAtendimento() {
-        this.opcaoSelecionada(0);//
-        System.out.println("[ 1 ] Atender");
-        System.out.println("[ 2 ] Ver próximo da fila");
-        System.out.println("[ 3 ] Ver fila de atendimento");
-        System.out.println("[ 4 ] Voltar");
-        System.out.println("Sua opção: ");
-    }
-    
-    public void mostrarFilaAtendimento() {
-    	this.opcaoSelecionada(3);//
+        System.out.println(OpcoesMenu.CABECALHO_MENU);
+        System.out.println("[ 1 ] " + OpcoesMenu.ATENDIMENTO);
+        System.out.println("[ 2 ] " + OpcoesMenu.SOLICITACOES);
+        System.out.println("[ 3 ] " + OpcoesMenu.SAIR);
+        System.out.println(OpcoesMenu.SUA_OPCAO);
     }
 
-    public void mostrarProximoFila() { this.opcaoSelecionada(4);}//
-    
+    public void menuAtendimento() {
+        this.opcaoSelecionada(OpcoesMenu.ATENDIMENTO);
+        System.out.println("[ 1 ] " + OpcoesMenu.ATENDER);
+        System.out.println("[ 2 ] " + OpcoesMenu.VER_PROXIMO_FILA);
+        System.out.println("[ 3 ] " + OpcoesMenu.VER_FILA);
+        System.out.println("[ 4 ] " + OpcoesMenu.VOLTAR);
+        System.out.println(OpcoesMenu.SUA_OPCAO);
+    }
+
+    public void mostrarFilaAtendimento() {
+        this.opcaoSelecionada(OpcoesMenu.VER_FILA);
+    }
+
+    public void mostrarProximoFila() {
+        this.opcaoSelecionada(OpcoesMenu.VER_PROXIMO_FILA);
+    }
+
     public void menuAtender() {
-    	this.opcaoSelecionada(2);//
+        this.opcaoSelecionada(OpcoesMenu.ATENDER);
     }
-    
+
     public void menuPassarParaAtendimento() {
-    	this.opcaoSelecionada(5);//
+        this.opcaoSelecionada(OpcoesMenu.PASSAR_PARA_ATENDIMENTO);
     }
-    
+
     public void menuSolicitacoes() {
-    	this.opcaoSelecionada(1);//
-        System.out.println("[ 1 ] Nova solicitação");
-        System.out.println("[ 2 ] Ver próxima solicitação");
-        System.out.println("[ 3 ] Ver todas as solicitações");
-        System.out.println("[ 4 ] Voltar");
-        System.out.println("Sua opção: ");
+        this.opcaoSelecionada(OpcoesMenu.SOLICITACOES);
+        System.out.println("[ 1 ] " + OpcoesMenu.NOVA_SOLICITACAO);
+        System.out.println("[ 2 ] " + OpcoesMenu.VER_PROXIMA_SOLICITACAO);
+        System.out.println("[ 3 ] " + OpcoesMenu.VER_TODAS_SOLICITACOES);
+        System.out.println("[ 4 ] " + OpcoesMenu.VOLTAR);
+        System.out.println(OpcoesMenu.SUA_OPCAO);
     }
-    
+
     public void menuVerSolicitacao() {
-        System.out.println("[ 1 ] Adicionar à fila de atendimento");
-        System.out.println("[ 2 ] Deletar");
-        System.out.println("[ 3 ] Voltar");
-        System.out.println("Sua opção: ");
+        System.out.println("[ 1 ] " + OpcoesMenu.ADICIONAR_A_FILA);
+        System.out.println("[ 2 ] " + OpcoesMenu.DELETAR);
+        System.out.println("[ 3 ] " + OpcoesMenu.VOLTAR);
+        System.out.println(OpcoesMenu.SUA_OPCAO);
     }
-    
+
     public void menuNovaSolicitacao() {
-    	this.opcaoSelecionada(6);
+        this.opcaoSelecionada(OpcoesMenu.NOVA_SOLICITACAO);
     }
-    
-    public void opcaoSelecionada(int indice) {
-        System.out.println("\nOpção " + opcoes[indice].toLowerCase() + " selecionada.\n");
+
+    public void opcaoSelecionada(String texto) {
+        System.out.println("\nOpção " + texto.toLowerCase() + " selecionada.\n");
     }
 }
