d1fa866d6bcc49d45ec0fd97a9a0615bfe38a371
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2025-04-25 16:06:19 -0300

    Atualiza struct OpcoesMenu

--- src/sistema_atendimento_fila_e_pilha/OpcoesMenu.java
+++ src/sistema_atendimento_fila_e_pilha/OpcoesMenu.java
@@ -1,6 +1,8 @@
 package sistema_atendimento_fila_e_pilha;
 
 public class OpcoesMenu {
+    public static final String CABECALHO_MENU = "\n|--------MENU--------|";
+    public static final String INPUT_USUARIO_MENU = "Sua opção: ";
     public static final String ATENDIMENTO = "Atendimento";
     public static final String SOLICITACOES = "Solicitações";
     public static final String ATENDER = "Atender";
@@ -9,6 +11,13 @@ public class OpcoesMenu {
     public static final String PASSAR_PARA_ATENDIMENTO = "Passar para atendimento";
     public static final String NOVA_SOLICITACAO = "Nova solicitação";
     public static final String VER_TODAS_SOLICITACOES = "Ver todas as solicitações";
+    public static final String DELETAR = "Deletar";
+    public static final String VER_PROXIMA_SOLICITACAO = "Ver próxima solicitação";
+    public static final String ADICIONAR_A_FILA = "Adicionar à fila de atendimento";
+    public static final String SAIR = "Sair";
+    public static final String VOLTAR = "Voltar";
+    public static final String OPCAO_INVALIDA = "Opção inválida.";
+    public static final String PREENCHA_CAMPOS = "Complete os campos abaixo\n";
     public static final String RESPOSTA_CLIENTE = "Resposta ao cliente: ";
     public static final String ID_CLIENTE = "ID do cliente: ";
     public static final String DESCRICAO = "Insira a descrição da solicitação: ";
@@ -16,11 +25,5 @@ public class OpcoesMenu {
     public static final String HORA = "Hora: ";
     public static final String NOME_CLIENTE = "Nome do cliente: ";
     public static final String MOTIVO = "Motivo do atendimento: ";
-    public static final String DELETAR = "Deletar";
-    public static final String VER_PROXIMA_SOLICITACAO = "Ver próxima solicitação";
-    public static final String CABECALHO_MENU = "\n|--------MENU--------|";
-    public static final String SAIR = "Sair";
-    public static final String SUA_OPCAO = "Sua opção: ";
-    public static final String VOLTAR = "Voltar";
-    public static final String ADICIONAR_A_FILA = "Adicionar à fila de atendimento";
+    public static final String ENCERRA_PROGRAMA = "Programa encerrado, tenha um excelente dia!";
 }
